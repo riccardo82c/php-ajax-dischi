52982e4841aacb6591b33f584c4c9bcaf3367934
Riccardo Colombo <rikyregna@gmail.com>
2020-09-18 15:34:43 +0200

    init dischi-db.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+include __DIR__ .'/dischi-db.php';
+var_dump($database);
+  
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Db Dischi PHP</title>
+</head>
+<body>
+	
+</body>
+</html>
