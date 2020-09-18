d33dfbc2de295d8e24c2dcba6b9bc37db8b74901
Riccardo Colombo <rikyregna@gmail.com>
2020-09-18 16:19:32 +0200

    responsive layout

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 include __DIR__ .'/dischi-db.php';
-var_dump($database);
+/* var_dump($database); */
   
 ?>
 
@@ -9,12 +9,43 @@ var_dump($database);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="dist/app.css">
 	<title>Db Dischi PHP</title>
 </head>
+
 <body>
-	
+
+	<div class="container">
+
+		<header>
+			<nav>
+				<img src="https://www.freepnglogos.com/uploads/spotify-logo-png/icon-spotify-pace-house-2.png"
+					alt="icon spotify pace house" />
+
+			</nav>
+
+		</header>
+
+		<main>
+			<div class="disc"></div>
+			<div class="disc"></div>
+			<div class="disc"></div>
+			<div class="disc"></div>
+			<div class="disc"></div>
+			<div class="disc"></div>
+			<div class="disc"></div>
+			<div class="disc"></div>
+		</main>
+
+
+	</div>
+
+
+
 </body>
+
 </html>
